644e74babbea0ecd0fec85f49829291432884f38
Roger Pessoa <roger15p@hotmail.com>
2024-09-17 14:09:44 -0300

    Atualização dos arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,21 @@ import MyForm from "./components/MyForm";
 import { useState, useEffect } from "react";
 import { v4 } from "uuid";
 
-const url = "http://localhost:3000/contasAPagar";
+// const url = "http://localhost:3000/contasAPagar";
 
 function App() {
   const [despesas, setDespesas] = useState([]);
 
   useEffect(() => {
-    async function getData() {
-      const res = await fetch(url);
-
-      const data = await res.json();
-
-      setDespesas(data);
+    const storedDespesas = localStorage.getItem("despesas");
+    if (storedDespesas) {
+      setDespesas(JSON.parse(storedDespesas));
     }
-
-    getData();
   }, []);
 
   // envio de dados
 
-  const handleDespesas = async (e) => {
+  const handleDespesas = (e) => {
     e.preventDefault();
     const despesa = {
       id: v4(),
@@ -33,117 +28,91 @@ function App() {
       mes: e.target.mes.value,
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(despesa),
-    });
-
-    setDespesas((anterior) => [...anterior, despesa]);
+    setDespesas([...despesas, despesa]);
+    localStorage.setItem("despesas", JSON.stringify([...despesas, despesa]));
   };
 
   function pagar(index) {
     const item = despesas[index];
-    const itemId = item.id;
     const novasDespesas = [...despesas];
     novasDespesas[index].status = "pago";
     setDespesas(novasDespesas);
-
-    fetch(`${url}/${itemId}`, {
-      method: "PUT", // ou 'PATCH'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...item, status: "pago" }),
-
-    });
+    localStorage.setItem('despesas', JSON.stringify(novasDespesas));
   }
 
   function verificarStatusERetornarCor(status) {
-    if (status == "pendente") {
-      return "orange";
-    }
-    if (status == "pago") {
-      return "green";
+    switch (status) {
+      case "pendente":
+        return "orange";
+      case "pago":
+        return "green";
+      default:
+        return "gray";
     }
   }
 
   function extornar(index) {
-    const item = despesas[index];
-    const itemId = item.id;
+    const item = despesas[index]; 
     const novasDespesas = [...despesas];
     novasDespesas[index].status = "pendente";
     setDespesas(novasDespesas);
-
-    fetch(`${url}/${itemId}`, {
-      method: "PUT", // ou 'PATCH'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...item, status: "pendente" }),
-
-    });
+    localStorage.setItem('despesas', JSON.stringify(novasDespesas));
   }
 
   function deletar(index) {
     const item = despesas[index];
-    const itemId = item.id;
     const restantes = [...despesas];
+    const novasDespesas = [...despesas];
+
     restantes.splice(index, 1);
     setDespesas(restantes);
-
-    fetch(`${url}/${itemId}`, {
-      method: "DELETE",
-    });
-
+    localStorage.removeItem('despesas', JSON.stringify(novasDespesas));
   }
 
   return (
-    <>
-      <div className="header-container">
-        <h1>Controle Mensal</h1>
-      </div>
-      <MyForm handleDespesas={handleDespesas} />
-      <div className="despesas-container">
-        <ul>
-          {despesas.map((item, index) => (
-            <>
-              <div className="resposta-form">
-                <div
-                  style={{ color: verificarStatusERetornarCor(item.status) }}
-                  className="resultados"
-                >
-                  <li key={despesas.id}>Nome da despesa - {item.despesa}</li>
-                  <li key={despesas.id}>Valor - R$ {item.valor}</li>
-                  <li key={despesas.id}>Status - {item.status}</li>
-                  <li key={despesas.id}>Mês - {item.mes}</li>
-                </div>
-                <div className="botoes">
-                  <Button
-                    onclick={() => pagar(index)}
-                    className="pago"
-                    name="Pago"
-                  />
-                  <Button
-                    onclick={() => extornar(index)}
-                    className="pendente"
-                    name={"Pendente"}
-                  />
-                  <Button
-                    onclick={() => deletar(index)}
-                    className="excluir"
-                    name={"Excluir"}
-                  />
-                </div>
-              </div>
-            </>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+    <>
+      <div className="header-container">
+        <h1>Controle Mensal</h1>
+      </div>
+      <MyForm handleDespesas={handleDespesas} />
+      <div className="despesas-container">
+        <ul>
+          {despesas.map((item, index) => (
+            <>
+              <div className="resposta-form">
+                <div
+                  style={{ color: verificarStatusERetornarCor(item.status) }}
+                  className="resultados"
+                >
+                  <li key={despesas.id}>Nome da despesa - {item.despesa}</li>
+                  <li key={despesas.id}>Valor - R$ {item.valor}</li>
+                  <li key={despesas.id}>Status - {item.status}</li>
+                  <li key={despesas.id}>Mês - {item.mes}</li>
+                </div>
+                <div className="botoes">
+                  <Button
+                    onclick={() => pagar(index)}
+                    className="pago"
+                    name="Pago"
+                  />
+                  <Button
+                    onclick={() => extornar(index)}
+                    className="pendente"
+                    name={"Pendente"}
+                  />
+                  <Button
+                    onclick={() => deletar(index)}
+                    className="excluir"
+                    name={"Excluir"}
+                  />
+                </div>
+              </div>
+            </>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
